4504af4cf33f3fc455cc62d6578faae02a1a80b0
abrinkmann <alex.brinkmann@informatik.uni-mannheim.de>
2024-06-10 10:07:45 +0200

    Reduce run_strategy script

--- src/scripts/05_run_strategy.sh
+++ src/scripts/05_run_strategy.sh
@@ -7,18 +7,8 @@ export CUDA_VISIBLE_DEVICES=5
 
 
 python src/strategy/run_strategy.py --path_to_config=$(pwd)'/config/experiments/abt-buy/blocking_experiments_abt-buy.yml' --worker=2
-#python src/strategy/run_strategy.py --path_to_config='/home/alebrink/development/sc-block/config/experiments/walmart-amazon_1/blocking_experiments_walmart-amazon_1.yml' --worker=4
-#python src/strategy/run_strategy.py --path_to_config='/home/alebrink/development/sc-block/config/experiments/amazon-google/blocking_experiments_amazon-google.yml' --worker=2
-#python src/strategy/run_strategy.py --path_to_config='/home/alebrink/development/sc-block/config/experiments/wdcproducts80cc20rnd050un_block_s_train_l/blocking_experiments_wdcproducts80cc20rnd050un_block_s_train_l.yml' --worker=4
-#python src/strategy/run_strategy.py --path_to_config='/home/alebrink/development/sc-block/config/experiments/wdcproducts80cc20rnd050un_block_m_train_l/blocking_experiments_wdcproducts80cc20rnd050un_block_m_train_l.yml' --worker=4
-#python src/strategy/run_strategy.py --path_to_config='/home/alebrink/development/sc-block/config/experiments/wdcproducts80cc20rnd050un_block_l_train_l/blocking_experiments_wdcproducts80cc20rnd050un_block_l_train_l.yml' --worker=4
-
-
-#python src/strategy/run_strategy.py --path_to_config='/home/alebrink/development/sc-block/config/experiments/abt-buy/blocking_experiments_abt-buy_switched.yml' --worker=4
-#python src/strategy/run_strategy.py --path_to_config='/home/alebrink/development/sc-block/config/experiments/amazon-google/blocking_experiments_amazon-google_switched.yml' --worker=4
-#python src/strategy/run_strategy.py --path_to_config='/home/alebrink/development/sc-block/config/experiments/walmart-amazon_1/blocking_experiments_walmart-amazon_1_switched.yml' --worker=4
-#python src/strategy/run_strategy.py --path_to_config='/home/alebrink/development/sc-block/config/experiments/wdcproducts80cc20rnd050un_block_s_train_l/blocking_experiments_wdcproducts80cc20rnd050un_block_s_train_l_switched.yml' --worker=4
-#python src/strategy/run_strategy.py --path_to_config='/home/alebrink/development/sc-block/config/experiments/wdcproducts80cc20rnd050un_block_m_train_l/blocking_experiments_wdcproducts80cc20rnd050un_block_m_train_l_switched.yml' --worker=4
-#python src/strategy/run_strategy.py --path_to_config='/home/alebrink/development/sc-block/config/experiments/wdcproducts80cc20rnd050un_block_l_train_l/blocking_experiments_wdcproducts80cc20rnd050un_block_l_train_l_switched.yml' --worker=4
-
-
+#python src/strategy/run_strategy.py --path_to_config=$(pwd)'/config/experiments/walmart-amazon_1/blocking_experiments_walmart-amazon_1.yml' --worker=4
+#python src/strategy/run_strategy.py --path_to_config=$(pwd)'config/experiments/amazon-google/blocking_experiments_amazon-google.yml' --worker=2
+#python src/strategy/run_strategy.py --path_to_config=$(pwd)'config/experiments/wdcproducts80cc20rnd050un_block_s_train_l/blocking_experiments_wdcproducts80cc20rnd050un_block_s_train_l.yml' --worker=4
+#python src/strategy/run_strategy.py --path_to_config=$(pwd)'sc-block/config/experiments/wdcproducts80cc20rnd050un_block_m_train_l/blocking_experiments_wdcproducts80cc20rnd050un_block_m_train_l.yml' --worker=4
+#python src/strategy/run_strategy.py --path_to_config=$(pwd)'/config/experiments/wdcproducts80cc20rnd050un_block_l_train_l/blocking_experiments_wdcproducts80cc20rnd050un_block_l_train_l.yml' --worker=4
